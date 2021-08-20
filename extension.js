946cdd54f3b72b6dd80ac315c95d8b849530e2e2
Robby Bennett <robbycbennett@gmail.com>
2021-08-20 15:59:04 -0600

    Got rid of the trash

--- extension.js
+++ extension.js
@@ -1,33 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
+// Install
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "terminal-macros" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	// Implement commands from package.json
 	let disposable = vscode.commands.registerCommand('terminal-macros.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Terminal Macros!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+// Uninstall
 function deactivate() {}
 
 module.exports = {
