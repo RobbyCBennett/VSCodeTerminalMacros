7977bcd1cf9f9fc9c0cb199b12cd72efa72bfc5b
Robby Bennett <robbycbennett@gmail.com>
2021-08-21 00:34:30 -0600

    Getting closer

--- extension.js
+++ extension.js
@@ -1,14 +1,27 @@
 const vscode = require('vscode');
 
+// vscode.window.showInformationMessage('Info');
+// vscode.window.showErrorMessage('Error');
+
 // Install
 function activate(context) {
 	// Implement commands here, defined in package.json
 	commands = vscode.commands.registerCommand('terminalMacros.executeCommand', () => {
-		if (vscode.window.activeTerminal) {
-			// lastCommand = vscode.workspace.getConfiguration('TerminalMacros').get<string>('LastCommand');
-			vscode.window.showInformationMessage('Executing Command');
-			// vscode.window.activeTerminal.sendText(lastCommand, true);
+		// Options
+		save = true;
+		clear = true;
+		execute = true; // Implemented
+		focus = true; // Almost implemented
+
+		// Get terminal
+		terminal = vscode.window.activeTerminal;
+		if (!terminal) {
+			terminal = vscode.window.createTerminal();
 		}
+
+		// Send the command
+		terminal.show(!focus);
+		terminal.sendText('echo "Hello world :)"', execute);
 	});
 
 	context.subscriptions.push(commands);
