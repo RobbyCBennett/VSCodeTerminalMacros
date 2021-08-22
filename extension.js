c84e4b4ba292313a64d5737400c6e272878417c4
Robby Bennett <robbycbennett@gmail.com>
2021-08-22 10:16:02 -0600

    Implemented command settings

--- extension.js
+++ extension.js
@@ -3,25 +3,83 @@ const vscode = require('vscode');
 // vscode.window.showInformationMessage('Info');
 // vscode.window.showErrorMessage('Error');
 
+//		Useful keys:
+//		\u001b		Escape
+//		\u001b[A	Up
+//		\01			Select all
+//		\03			Stop Process
+//		\04			Logout
+//		\10			Backspace
+//		\13			Clear line before cursor
+//		\14			Clear terminal
+//		\26			Paste
+//		\27			Clear line after cursor
+
+function prepareTerminal(terminal, stop, logout, clear, execute, command) {
+	// Clear line
+	if (terminal.name == 'cmd' || terminal.name == 'powershell') {
+		terminal.sendText('\u001b', false);
+	} else {
+		terminal.sendText('\13\27', false);
+	}
+
+	// Stop
+	if (stop) {
+		terminal.sendText('\03', false);
+	}
+
+	// Logout
+	if (logout) {
+		terminal.sendText('\04', false);
+	}
+
+	// Clear terminal
+	if (clear) {
+		if (terminal.name == 'cmd') {
+			terminal.sendText('cls');
+		}
+		else {
+			terminal.sendText('\14', false);
+		}
+	}
+
+	// Send command
+	terminal.sendText(command, execute);
+}
+
 // Install
 function activate(context) {
 	// Implement commands here, defined in package.json
 	commands = vscode.commands.registerCommand('terminalMacros.executeCommand', () => {
 		// Options
 		save = true;
+		stop = false;
+		logout = false;
 		clear = true;
-		execute = true; // Implemented
-		focus = true; // Almost implemented
+		execute = false;
+		focus = false; // Buggy when terminal is hidden. Terminal.show(preserveFocus: true) doesn't work
+		command = '\u001b[A';
 
 		// Get terminal
 		terminal = vscode.window.activeTerminal;
 		if (!terminal) {
 			terminal = vscode.window.createTerminal();
 		}
+		terminal.show(!focus);
 
-		// Send the command
-		terminal.show(focus);
-		terminal.sendText('echo "Hello world :)"', execute);
+		// Fix the recent command if using cmd and clearing the terminal
+		if (terminal.name == 'cmd' && clear && command == '\u001b[A') {
+			command = '\u001b[A\u001b[A';
+		}
+
+		// Save and prepare terminal
+		if (save) {
+			vscode.window.activeTextEditor.document.save().then(() => {
+				prepareTerminal(terminal, stop, logout, clear, execute, command);
+			});
+		} else {
+			prepareTerminal(terminal, stop, logout, clear, execute, command);
+		}
 	});
 
 	context.subscriptions.push(commands);
